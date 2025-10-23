160807aaab38067fb2f6373d7dfdd413bf020381
Raquel Santos <raquelmrsantos@gmail.com>
2025-10-23 15:30:14 +0200

    Change commented code in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,39 +5,39 @@ import Experience from '@/components/Experience';
 import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
-//import LoadingAnimation from '@/components/LoadingAnimation';
+import LoadingAnimation from '@/components/LoadingAnimation';
 import ConnectForm from '@/components/ConnectForm';
 
 export default function Home() {
   return (
     <>
-      {/*     <LoadingAnimation> */}
-      <main>
-        <Navbar />
+      <LoadingAnimation>
+        <main>
+          <Navbar />
 
-        <section id='home'>
-          <Hero />
-        </section>
+          <section id='home'>
+            <Hero />
+          </section>
 
-        <section id='about'>
-          <About />
-        </section>
+          <section id='about'>
+            <About />
+          </section>
 
-        <section id='experience'>
-          <Experience />
-        </section>
+          <section id='experience'>
+            <Experience />
+          </section>
 
-        <section id='connect'>
-          <ConnectForm />
-        </section>
+          <section id='connect'>
+            <ConnectForm />
+          </section>
 
-        <section id='contact'>
-          <Contact />
-        </section>
-      </main>
-      <Footer />
-      <BackToTop />
-      {/*  </LoadingAnimation> */}
+          <section id='contact'>
+            <Contact />
+          </section>
+        </main>
+        <Footer />
+        <BackToTop />
+      </LoadingAnimation>
     </>
   );
 }
