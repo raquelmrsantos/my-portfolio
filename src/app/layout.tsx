9b66770d0a4242a9d3de13dd35e758af644ca4bc
Raquel Santos <raquelmrsantos@gmail.com>
2025-10-24 14:52:57 +0200

    Adds vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from 'next';
 import {
   Stint_Ultra_Expanded,
@@ -97,6 +98,7 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
