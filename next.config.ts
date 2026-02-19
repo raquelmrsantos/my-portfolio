4e9a11efadde66b1a7ae7eb23c4085f974744967
Raquel Santos <raquelmrsantos@gmail.com>
2026-02-19 22:47:37 +0000

    chore: remove webpack config causing build errors

--- next.config.ts
+++ next.config.ts
@@ -6,18 +6,6 @@ const nextConfig: NextConfig = {
     // Remove console.logs in production
     removeConsole: process.env.NODE_ENV === 'production',
   },
-
-  // Font loader configuration
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/fonts/[name][ext]',
-      },
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
